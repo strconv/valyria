89bfddc3033550d312c1428a0000c8c7937cce18
Aris <i@ariser.cn>
2020-05-11 16:06:16 +0800

    gin log

---
+++ log/ginLog.go
@@ -0,0 +1,32 @@
+package log
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GinLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 开始时间
+		start := time.Now()
+		// 处理请求
+		c.Next()
+		// 结束时间
+		end := time.Now()
+		//执行时间
+		latency := end.Sub(start)
+
+		path := c.Request.URL.Path
+
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+		For(c, "gin", "log").Infof("| %3d | %13v | %15s | %s  %s |",
+			statusCode,
+			latency,
+			clientIP,
+			method, path,
+		)
+	}
+}
