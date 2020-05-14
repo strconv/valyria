65c546cfda9bc752ec837dfad0b0d49a749c99e5
Aris <i@ariser.cn>
2020-05-14 16:27:45 +0800

    hash api

--- cache/redis/common.go
+++ cache/redis/common.go
@@ -40,11 +40,54 @@ func Delete(key string) error {
 	return _redis.Delete(key)
 }
 
+func HDel(key interface{}, fields ...interface{}) (res int, err error) {
+	var reply interface{}
+	keys := []interface{}{key}
+	keys = append(keys, fields...)
+
+	reply, err = _redis.Do("HDEL", keys)
+	if err != nil {
+		return
+	}
+	res = reply.(int)
+	return
+}
+
+func HSet(key, field string, value interface{}) (res int, err error) {
+	var reply interface{}
+	reply, err = _redis.Do("HSET", key, field, value)
+	if err != nil {
+		return
+	}
+	res = reply.(int)
+	return
+}
+
+func HGet(key, field string) (res string, err error) {
+	var reply interface{}
+	reply, err = _redis.Do("HGET", key, field)
+	if err != nil {
+		return
+	}
+	res = reply.(string)
+	return
+}
+
+func HGetInt(key, field string) (res int, err error) {
+	var reply interface{}
+	reply, err = _redis.Do("HGET", key, field)
+	if err != nil {
+		return
+	}
+	res = reply.(int)
+	return
+}
+
 func HMSet(key string, fields ...interface{}) (res string, err error) {
 	var reply interface{}
 	keys := []interface{}{key}
 	keys = append(keys, fields)
-	reply, err = _redis.Do("HMSET", key, fields)
+	reply, err = _redis.Do("HMSET", keys)
 	res = reply.(string)
 	return
 }
